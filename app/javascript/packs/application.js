1053e24a2e0c17446c6557e25a0c7d52ff3cdffb
Alex Cattell <abcattell91@gmail.com>
2022-03-28 19:11:07 +0100

    add bootstrap, jquery and popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
+require("bootstrap")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
