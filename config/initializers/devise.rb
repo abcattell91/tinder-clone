577da7a1350a8c51008a9a750aea5675cb2c1dfe
Alex Cattell <abcattell91@gmail.com>
2022-03-30 20:15:37 +0100

    Updated some text  information

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -285,6 +285,7 @@ Devise.setup do |config|
   # end
 
 
+
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
