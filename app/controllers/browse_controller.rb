bedbdda4e2d6db7e87b534c84dba47933e78cadb
Alex Cattell <abcattell91@gmail.com>
2022-04-01 19:12:21 +0100

    Implemented a liked id

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -5,7 +5,22 @@ class BrowseController < ApplicationController
   end
 
   def approve
-    # user swipes right
+    account_id = params[:id]
+
+    logger.debug "User id for matching is #{account_id}"
+
+    # create like message for browser
+    new_like = Like.new(liked_account_id: account_id)
+    new_like.account_id = current_account.id
+
+    if new_like.save
+      # check if user already likes us back
+      existing_like = Like.where(account_id: account_id, liked_account_id: current_account.id).count
+      @they_like_us = existing_like > 0
+    else
+    # issue saving like - return a warning message
+    end
+
   end
 
   def decline
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_112321) do
+ActiveRecord::Schema.define(version: 2022_04_01_175249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 2022_03_31_112321) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "account_id"
+    t.integer "liked_account_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_likes_on_account_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
