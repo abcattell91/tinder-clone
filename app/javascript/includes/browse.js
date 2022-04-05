1dbf0095ebba07a0f40ac3af224c59f4dcfbbd89
Alex Cattell <abcattell91@gmail.com>
2022-04-05 13:53:07 +0100

    Formatting errors corrected

--- app/javascript/includes/browse.js
+++ app/javascript/includes/browse.js
@@ -4,6 +4,8 @@ $(function(){
 $(".match-tile").on("click", function(){
   var account_id = $(this).data("id");
   console.log( account_id );
+
+  $("#conversation").show();
 })
 
 $("#decline").on("click", function(){
